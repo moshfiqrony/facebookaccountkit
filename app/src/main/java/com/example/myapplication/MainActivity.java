9997cd13d931c0d94487ad5aaefe5aba1af3b4d5
moshfiqrony <moshfiqrony@gmail.com>
2019-05-12 13:08:14 +0600

    MR | Facebook Account Kit Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE, AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+        startActivityForResult(intent, 999);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 999){
+            AccountKitLoginResult res = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (res.getError() != null){
+                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+                return;
+            }else if(res.wasCancelled()){
+                Toast.makeText(this, ""+res.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                return;
+            }else {
+                startActivity(new Intent(this, Dashboard.class));
+            }
+        }
     }
 }

8a1b786093539731e515c88495c15ffc40fef91e
moshfiqrony <moshfiqrony@gmail.com>
2019-05-16 10:09:28 +0600

    MR | facebook account kit commented

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,28 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
-        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
-                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE, AccountKitActivity.ResponseType.TOKEN);
-        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
-        startActivityForResult(intent, 999);
+
+
+
+
+
+
+
+
+//        Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
+//        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+//                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE, AccountKitActivity.ResponseType.TOKEN);
+//        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+//        startActivityForResult(intent, 999);
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 999){
-            AccountKitLoginResult res = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
-            if (res.getError() != null){
-                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
-                return;
-            }else if(res.wasCancelled()){
-                Toast.makeText(this, ""+res.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
-                return;
-            }else {
-                startActivity(new Intent(this, BottomNavAct.class));
-            }
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == 999){
+//            AccountKitLoginResult res = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+//            if (res.getError() != null){
+//                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+//                return;
+//            }else if(res.wasCancelled()){
+//                Toast.makeText(this, ""+res.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+//                return;
+//            }else {
+//                startActivity(new Intent(this, BottomNavAct.class));
+//            }
+//        }
+//    }
 }
